40ea7898767b0c685a844a270269458314cf5873
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-07-16 21:13:45 +0200

    .

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    devServer: {
+        allowedHosts: [
+            'herokuapp.com',
+            'lords-mobile-tools.herokuapp.com/',
+            'localhost:3000',
+        ],
+    },
+};
