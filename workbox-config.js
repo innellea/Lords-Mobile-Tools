1b375ac4b25f00f5e3b4668dec50433613d1af3b
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-07-23 17:28:23 +0200

    fix: :bug: workbox cache issue
    attempting to resolve workbox cache cleanup

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,7 @@
 module.exports = {
-	globDirectory: 'public/',
-	globPatterns: [
-		'**/*.{png,ico,html,json,txt,js,css}'
-	],
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	],
-	swDest: 'public/sw.js'
+    globDirectory: 'public/',
+    globPatterns: ['**/*.{png,ico,html,json,txt,js,css}'],
+    ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+    cleanupOutdatedCaches: true,
+    swDest: 'public/sw.js',
 };
