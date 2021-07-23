08c2695ee86300988c4f583287e4608701d19852
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-07-23 17:40:07 +0200

    add
    ...

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,14 @@
 module.exports = {
+<<<<<<< Updated upstream
     globDirectory: 'build/',
     globPatterns: ['**/*.{html,json,js,css}'],
     swDest: 'build/sw.js',
+=======
+    globDirectory: 'public/',
+    globPatterns: ['**/*.{png,ico,html,json,txt,css,}'],
+    globIgnores: ['main.js'],
+    ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+>>>>>>> Stashed changes
     cleanupOutdatedCaches: true,
     // Define runtime caching rules.
 };
