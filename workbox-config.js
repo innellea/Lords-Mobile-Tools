37612b3c1201c55f2c0634bef1bd08b9bf95e9aa
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-07-23 17:58:33 +0200

    workbox fix #2

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,11 @@
 module.exports = {
     globDirectory: 'public/',
-    globPatterns: ['**/*.{png,ico,html,json,txt,css}'],
-    ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+    globPatterns: ['**/*.{png,ico,html,json,txt,js,css}'],
+    clientsClaim: true,
+    skipWaiting: true,
+    dontCacheBustURLsMatching: new RegExp('.+.[a-f0-9]{20}..+|index.html'),
     cleanupOutdatedCaches: true,
+    exclude: [new RegExp('index.html')],
+    ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
     swDest: 'public/sw.js',
 };
