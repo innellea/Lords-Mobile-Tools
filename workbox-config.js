8c6c858524da08a77bc933ee657691e6e912ae4b
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-07-23 22:51:18 +0200

    fix: :bug: workbox fix

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,7 @@
 module.exports = {
-    globDirectory: 'public/',
-    globPatterns: ['**/*.{png,ico,html,json,txt,js,css}'],
-    clientsClaim: true,
+    globDirectory: 'src/',
+    globPatterns: ['**/*.{png,ico,html,json,txt,js,css,jsx,svg,jpg}'],
     skipWaiting: true,
-    dontCacheBustURLsMatching: new RegExp('.+.[a-f0-9]{20}..+|index.html'),
-    cleanupOutdatedCaches: true,
-    exclude: [new RegExp('index.html')],
     ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-    swDest: 'public/sw.js',
+    swDest: 'src/sw.js',
 };
