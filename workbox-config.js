4338c5d96c5c09672343fcf39abee8d1e0e85805
0xFFFF <43180177+innellea@users.noreply.github.com>
2021-07-24 01:16:23 +0200

    fix: :bug: workbox fix #4

--- workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    globDirectory: 'src/',
-    globPatterns: ['**/*.{png,ico,html,json,txt,js,css,jsx,svg,jpg}'],
-    skipWaiting: true,
-    ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-    swDest: 'src/sw.js',
-};
